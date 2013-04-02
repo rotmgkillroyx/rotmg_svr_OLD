3de139982fee867c82b292ee251ed3d3df3124fb
K <lavacreepy@gmail.com>
2013-04-02 13:33:24 +0800

    add some commands

--- wServer/realm/entities/player/commands/WorldCommand.cs
+++ wServer/realm/entities/player/commands/WorldCommand.cs
@@ -9,11 +9,7 @@ namespace wServer.realm.entities.player.commands
     class TutorialCommand : ICommand
     {
         public string Command { get { return "tutorial"; } }
-
-        public bool RequirePerm
-        {
-            get { return true; }
-        }
+        public bool RequirePerm { get { return false; } }
 
         public void Execute(Player player, string[] args)
         {
@@ -31,11 +27,7 @@ namespace wServer.realm.entities.player.commands
     class WhoCommand : ICommand
     {
         public string Command { get { return "who"; } }
-
-        public bool RequirePerm
-        {
-            get { return true; }
-        }
+        public bool RequirePerm { get { return false; } }
 
         public void Execute(Player player, string[] args)
         {
@@ -43,7 +35,7 @@ namespace wServer.realm.entities.player.commands
             var copy = player.Owner.Players.Values.ToArray();
             for (int i = 0; i < copy.Length; i++)
             {
-                if (i != 0)sb.Append(", ");
+                if (i != 0) sb.Append(", ");
                 sb.Append(copy[i].Name);
             }
 
@@ -54,6 +46,74 @@ namespace wServer.realm.entities.player.commands
                 Name = "",
                 Text = sb.ToString()
             });
+        }
+    }
+
+    class ServerCommand : ICommand
+    {
+        public string Command { get { return "server"; } }
+        public bool RequirePerm { get { return false; } }
+
+        public void Execute(Player player, string[] args)
+        {
+            player.Client.SendPacket(new TextPacket()
+            {
+                BubbleTime = 0,
+                Stars = -1,
+                Name = "",
+                Text = player.Owner.Name
+            });
+        }
+    }
+
+    class PauseCommand : ICommand
+    {
+        public string Command { get { return "pause"; } }
+        public bool RequirePerm { get { return false; } }
+
+        public void Execute(Player player, string[] args)
+        {
+            if (player.HasConditionEffect(ConditionEffects.Paused))
+            {
+                player.ApplyConditionEffect(new ConditionEffect()
+                {
+                    Effect = ConditionEffectIndex.Paused,
+                    DurationMS = 0
+                });
+                player.Client.SendPacket(new TextPacket()
+                {
+                    BubbleTime = 0,
+                    Stars = -1,
+                    Name = "",
+                    Text = "Game resumed."
+                });
+            }
+            else
+            {
+                if (player.Owner.EnemiesCollision.HitTest(player.X, player.Y, 8).OfType<Enemy>().Any())
+                    player.Client.SendPacket(new TextPacket()
+                    {
+                        BubbleTime = 0,
+                        Stars = -1,
+                        Name = "*Error*",
+                        Text = "Not safe to pause."
+                    });
+                else
+                {
+                    player.ApplyConditionEffect(new ConditionEffect()
+                    {
+                        Effect = ConditionEffectIndex.Paused,
+                        DurationMS = -1
+                    });
+                    player.Client.SendPacket(new TextPacket()
+                    {
+                        BubbleTime = 0,
+                        Stars = -1,
+                        Name = "",
+                        Text = "Game paused."
+                    });
+                }
+            }
         }
     }
 }
